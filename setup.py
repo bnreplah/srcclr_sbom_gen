db5b951d181d4c675b8a7d49e416dad719f4e3f8
Ben H <46547251+bnreplah@users.noreply.github.com>
2025-01-27 05:25:39 -0800

    Update setup.py
    made updates to maintainer info

--- setup.py
+++ setup.py
@@ -5,8 +5,8 @@ from srcclr_sbom_gen.srcclr_sbom_gen import __version__
 setup(
     name="srcclr_sbom_gen",
     version=__version__,
-    author="Hanley Shun",
-    author_email="hshun@veracode.com",
+    author="",
+    author_email="",
     description="Parses srcclr scan results and converts to CycloneDX [https://cyclonedx.org/] SBOM JSON format.",
     url="https://github.com/bnreplah/srcclr_sbom_gen",
     license="Veracode",
